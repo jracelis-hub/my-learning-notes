7cd844a7dc31c7cd5800ebaae26f477a9e209669
jracelis-hub <jarron.racelis@gmail.com>
2025-08-14 13:39:30 -0700

    Update

---
+++ c_programming/examples-src/c-string-examples/my_string.h
@@ -0,0 +1,8 @@
+#ifndef                 _MY_STRING_H
+#define                 _MY_STRING_H
+
+int my_str_len(const char *string);
+char *my_str_tok(char *string,const char tok);
+void my_str_cpy(char *dst, const char *src);
+
+#endif               /* _MY_STRING_H */
