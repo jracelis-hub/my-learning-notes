3b3bf232a3049c37bb2df44c08d7231c04a75f20
Jarron Racelis <jarron.racelis@gmail.com>
2025-09-25 11:24:20 -0700

    update

--- c_programming/examples-src/strings-lib/my_string.h
+++ c_programming/examples-src/strings-lib/my_string.h
@@ -2,5 +2,6 @@
 #define MY_STRING_H
 
 unsigned long string_length(const char *string);
+void *memoryset(void *s, int bytes, unsigned long size);
 
 #endif /* MY_STRING_H */
--- c_programming/examples-src/strings-lib/strings.c
+++ c_programming/examples-src/strings-lib/strings.c
@@ -1,13 +1,22 @@
 #include "my_string.h"
+#include <stdio.h>
 
 unsigned long string_length(const char *string) {
 
-	unsigned long s = 0;
-	int i;
+	unsigned long i = 0;
 
-	for (i=0; *(string+i) != '\0';i++) {
-		s++;
-	}
+	for (i=0; *(string+i) != '\0'; i++)
+		;
 
-	return s;
+	return i;
+}
+
+int main()
+{
+	const char *words = "Words\n";
+	unsigned long len = string_length(words);
+	
+	printf("Count = %lu\n",len);
+	
+	return 0;
 }
